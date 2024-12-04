c34e4e39919bbb09dad8d862ce09afb1f9a12d17
Mohammad Shahabuddin <153608835+xshshahab@users.noreply.github.com>
2024-12-04 23:23:32 +0530

    Update Colors.jsx

--- src/components/Colors/Colors.jsx
+++ src/components/Colors/Colors.jsx
@@ -3,8 +3,11 @@ import React from "react"
 const Colors = () => {
     return (
         <div className="w-full mt-14 min-h-screen">
-            Colors
-            <input type="color" name="" id="" />
+            <div className="flex items-center justify-center">
+                <p className="text-sky-500 text-xl">Colors</p>
+            <input type="color" name="color" id="color" />
+                <p>Copy & paste Tailwind CSS colors.</p>
+            </div>
         </div>
     );
 }
